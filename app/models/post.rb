91ca1b59c55bd628f284e359463cf23698513c52
Tyler Small <intrasolarmusic@gmail.com>
2017-01-29 23:28:27 -0600

    Change default URL to point to rails assets and path to public

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
-	has_attached_file :photo
+	has_attached_file :photo, 
+										:url  => "/assets/images/users/:id/:style/:basename.:extension",
+                  	:path => ":rails_root/public/assets/images/users/:id/:style/:basename.:extension"
+	
 	validates_attachment :photo, content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
