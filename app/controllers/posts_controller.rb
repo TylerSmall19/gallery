3c661d94531756f4c49fecae1f4c77edf8bcf0ad
Tyler Small <intrasolarmusic@gmail.com>
2017-01-30 13:16:09 -0600

    Handle post errors (#2)
    * Generate views and controllers for posts
    
    * Create overall achitecture for image adding.
    
    * Add support for file uploads.
    
    * Continue to track down bugs in photo form
    
    * Change default URL to point to rails assets and path to public
    
    * Resolve issues with Content type not what was expected.
    
    * Setup partial to handle error views
    
    * Allow proper error handling with validations

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  def new
+  end
+
+  def show
+    begin
+      @image = Post.find_by(id: params[:id]).photo
+    rescue
+      redirect_to new_post_path
+    end
+  end
+
+  def index
+  end
+
+  def create
+  	post = Post.new(post_params)
+    p post
+  	if post.save
+  		redirect_to post
+  	else
+  		@errors = post.errors.full_messages
+      p @errors
+  		render :new
+  	end
+  end
+
+  private
+  def post_params
+  	params.require(:post).permit(:title, :description, :photo)
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+	has_attached_file :photo, 
+										:url  	=> "/assets/images/users/:id/:style/:basename.:extension",
+                  	:path 	=> ":rails_root/public/assets/images/users/:id/:style/:basename.:extension"
+
+	validates :title, :description, :photo, presence: true
+	validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
